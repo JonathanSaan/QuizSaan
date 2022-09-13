1417930959175a3732cf476d7478764cfa34679e
JonathanSaan <crosspool03@gmail.com>
2022-09-13 11:42:10 -0300

    up

--- styles/home.ts
+++ styles/home.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 0 2rem;
+  padding: 0 2em;
 `;
 
 export const Main = styled.div`
@@ -12,11 +12,21 @@ export const Main = styled.div`
   min-height: 100vh;
   
   h1 {
+    font-size: 3em;
     margin: 0 0 20vh 0;
   }
   
   button {
     border: 1px solid gray;
     padding: 1em 2em;
+  }
+  
+  button:hover {
+    transition: 300ms;
+    transform: scale(0.98);
+  }
+  
+  button:active {
+    filter: Brightness(0.8);
   }
 `;
