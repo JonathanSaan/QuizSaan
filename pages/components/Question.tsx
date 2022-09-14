6a6ed3d17bfe77b0426b83eaa11c7c9513c408c8
JonathanSaan <crosspool03@gmail.com>
2022-09-14 11:32:59 -0300

    up

--- pages/components/Question.tsx
+++ pages/components/Question.tsx
@@ -1,20 +1,25 @@
-import Fade from "react-reveal/Fade";
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import { Main } from "../../styles/home";
 
 
 const Question = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+  
   return (
-    <Fade>
-      <Main>
-        <h1>
-          Question 1
-        </h1>
-        <button>
-          answer 1
-        </button>
-      </Main>
-    </Fade>
+    <Main data-aos="fade-in">
+      <h1>
+        Question 1
+      </h1>
+      <button>
+        answer 1
+      </button>
+    </Main>
   );
 };
 
